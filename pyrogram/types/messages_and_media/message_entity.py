8c259ff87d5572644d374bd438f4bd436d3d0e72
penggrin12 <miner.sidor@gmail.com>
2026-07-13 01:12:48 +0300

    fix: MessageEntity._parse return type is Optional

--- pyrogram/types/messages_and_media/message_entity.py
+++ pyrogram/types/messages_and_media/message_entity.py
@@ -92,7 +92,7 @@ class MessageEntity(Object):
         self.date_time_format = date_time_format
 
     @staticmethod
-    def _parse(client, entity: "raw.base.MessageEntity", users: dict) -> Optional["MessageEntity"]:
+    def _parse(client, entity: "raw.base.MessageEntity", users: dict[int, raw.base.User]) -> "MessageEntity":
         user_id = None
         unix_time = None
         date_time_format = None
